8db23f083a953aff0032667ddc66abc205b2cb25
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-03-11 11:17:38 +0300

    do first c task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+	*  - 
+	* Description: 
+	* Return: 
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
