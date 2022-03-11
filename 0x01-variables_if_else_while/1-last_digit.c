f5a31b6a372dfd9cd8be5612543ebbf6baf14d72
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-03-11 12:23:49 +0300

    fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %i is %i and is %s", n, n % 10, n % 10 > 5 ?
+	printf("Last digit of %i is %i and is %s\n", n, n % 10, n % 10 > 5 ?
 			"greater than 5" : n % 10 == 0 ? "0" : "less than 6 and not 0");
 	return (0);
 }
