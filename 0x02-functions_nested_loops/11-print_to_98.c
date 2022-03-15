cfdc1a0ac9b23a5d0ace6303c74382b0e7e65d05
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-03-15 13:52:17 +0300

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,30 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		if (n == 98)
+		while (n <= 98)
 		{
-			printf("%i\n", n++);
-		} else
+			if (n == 98)
+			{
+				printf("%i\n", n++);
+			} else
+			{
+				printf("%i, ", n++);
+			}
+		}
+	}
+	else
+	{
+		while (n >= 98)
 		{
-			printf("%i, ", n++);
+			if (n == 98)
+			{
+				printf("%i\n", n--);
+			} else
+			{
+				printf("%i, ", n--);
+			}
 		}
 	}
 }
