c43a1e9ad1a806a551d54495d2a479a351782599
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-03-15 12:40:52 +0300

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,14 @@
 #include "main.h"
 
 
-int _islower(int c) {
-	return c >= 'a' && c <= 'z';
+/**
+	* _islower - checks if integer is a lowercase letter
+	* @c: letter
+	* Description: checks if integer is a lowercase letter
+	* Return: boolean
+*/
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
 }
