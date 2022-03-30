4de5225bbdfac4897f9840627e78415e4163de72
Milkias Yeheyis <milkias.yeheyis@yahoo.com>
2022-03-30 12:32:04 +0300

    fix

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+	* _sqrt_recursion - returns natural square root of a number
+	* @n: Number to give the square root of
+	* Description: returns natural square root of a number
+	* Return: int
+*/
+int _sqrt_recursion(int n)
+{
+
+}
